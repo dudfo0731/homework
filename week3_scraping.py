27f208eb72eda18854c444fb4842d019eb5b08c3
dudfo0731 <71322126+dudfo0731@users.noreply.github.com>
2020-10-15 22:30:37 +0900

    Update week3_scraping.py
    짭잘한맛

--- week3_scraping.py
+++ week3_scraping.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 # 아래 빈 칸('')을 채워보세요
@@ -14,3 +19,10 @@ for tr in trs:
     title = tr.select_one('td.info > a.title').text.strip()
     artist = tr.select_one('td.info > a.artist').text
     print(rank, title, artist)
+
+    doc = {
+        'rank': rank,
+        'title': title,
+        'star': artist
+    }
+    db.musics.insert_one(doc)
